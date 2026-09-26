382a9173d9fde9d597ea9ab262d711d671716e35
Ratman <149208690+Ratman141@users.noreply.github.com>
2026-09-27 00:43:12 +0200

    Created set_init and set_free function for parser's indexSet (jump table)

--- src/include/parser.h
+++ src/include/parser.h
@@ -1,4 +1,17 @@
 #ifndef TAPEWORM_PARSER_H
 #define TAPEWORM_PARSER_H
 
+#include "lexer.h"
+
+typedef struct {
+    int *set;
+    size_t size;
+}indexSet;
+
+indexSet set_init(const vector *v);
+
+void set_free(indexSet *set);
+
+void parse(vector *v, indexSet *set);
+
 #endif //TAPEWORM_PARSER_H
--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,22 @@
 #include "include/parser.h"
+#include <stdlib.h>
+
+indexSet set_init(const vector *v) {
+    indexSet set;
+    set.set = malloc(v->size * sizeof(int));
+    set.size = v->size;
+    for (int i = 0; i < set.size; i++) {
+        set.set[i] = -1;
+    }
+    return set;
+}
+
+void set_free(indexSet *set) {
+    free(set);
+    set->set = NULL;
+    set->size = 0;
+}
+
+void parse(vector *v, indexSet *set) {
+
+}
